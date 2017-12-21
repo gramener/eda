1e2d246208b1fab1de4a487f9d78f599d080ac57
Nolan Cardozo <nolancardozo@gmail.com>
2017-12-21 20:22:51 +0530

    Documentation corrected

--- R/eda_metadata.R
+++ R/eda_metadata.R
@@ -4,9 +4,6 @@ eda_metadata <- function(data = NULL,file_info = NULL,columns = NULL){
     if (any(type %in% "numeric")) {
       vars <- c(vars,names(df)[sapply(df,is.numeric)])
     }
-    if (any(type %in% "factor")) {
-      vars <- c(vars,names(df)[sapply(df,is.factor)])
-    }
     if (any(type %in% "character")) {
       vars <- c(vars,names(df)[sapply(df,is.character)])
     }

64f9f3f2e957431f8b7bc6726a6193422906471d
Nolan Cardozo <nolancardozo@gmail.com>
2018-01-02 00:29:16 +0530

    commit

--- tests/testthat/test_metadata.R
+++ tests/testthat/test_metadata.R
@@ -1,13 +1,11 @@
 library(eda)
+library(yaml)
 context("Meta data")
 
 test_that("Metadata YAML file is consistent", {
-  eda <- eda_metadata$new(path = paste("C:/Users/Nolan/Desktop/eda/data/samples/csv files","bank.csv",sep = "/"))
-  expect_equal(mget(columns, envir=eda),read_yaml(paste("C:/Users/Nolan/Desktop/eda/data/samples/yaml outputs/","bank.csv",".yaml",sep = "")))
-  eda <- eda_metadata$new(path = paste("C:/Users/Nolan/Desktop/eda/data/samples/csv files","barc-hair-care-brands.csv",sep = "/"))
-  expect_equal(mget(columns, envir=eda),read_yaml(paste("C:/Users/Nolan/Desktop/eda/data/samples/yaml outputs/","barc-hair-care-brands.csv",".yaml",sep = "")))
-  eda <- eda_metadata$new(path = paste("C:/Users/Nolan/Desktop/eda/data/samples/csv files","nas.csv",sep = "/"))
-  expect_equal(mget(columns, envir=eda),read_yaml(paste("C:/Users/Nolan/Desktop/eda/data/samples/yaml outputs/","nas.csv",".yaml",sep = "")))
-})
+  expect_equal(mget(c("Description","Source","Row_description","Sampling_method","Prepared_by","Prepared_on","Format",
+                      "File_name","File_size","Encoding","Row_count","Column_count","Modified_on","columns"), envir=eda_metadata$new(path = paste("C:/Users/Nolan/Desktop/eda/data/samples/csv files","bank.csv",sep = "/"))),read_yaml(paste("C:/Users/Nolan/Desktop/eda/data/samples/yaml outputs/","bank.csv",".yaml",sep = "")))
+}
+)
 
 

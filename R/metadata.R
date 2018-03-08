dcbce007fb40d8b9f88532adccb02973a9ef1340
Nolan Cardozo <nolancardozo@gmail.com>
2018-03-08 13:28:50 +0530

    Update metadata.R
    fixed errors

--- R/metadata.R
+++ R/metadata.R
@@ -140,7 +140,7 @@ metadata <- R6Class(
         self$prepared_on <- as.character(Sys.Date())
         self$format <- as.character(file_info$file_info$file.ext)
         self$file_name <- as.character(file_info$file_info$file.name)
-        self$file_size <-  utils:::format.object_size(file_info$file_info$file.size, "auto")
+        self$file_size <-  utils:::format.object_size(as.numeric(strsplit(as.character(file_info$file_info$file.size), " ")[[1]][1]), "auto")
         self$encoding <- as.character(file_info$file_info$file.encoding)
         self$row_count <- file_info$file_info$number.of.rows
         self$column_count <- file_info$file_info$number.of.columns
@@ -338,3 +338,5 @@ metadata <- R6Class(
     }
   )
 )
+
+
